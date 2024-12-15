c8f6edfa16abf0872899106496219fa6c31dc40c
Kacper Zajac <kacper.zajac004@gmail.com>
2024-12-15 22:25:41 +0100

    lab2 - small static website project

---
+++ Lab_02 - Mikroprojekt/assets/js/form.js
@@ -0,0 +1,58 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+
+function checkInput() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errorMsgEmail = document.querySelector(".error-msg.email");
+
+    if (!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorMsgEmail.innerText = "Email jest nieprawidłowy";
+        }
+        else {
+            errorMsgEmail.innerText = "Email nie może być pusty";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInput();
+})
